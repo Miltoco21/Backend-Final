c136b53636095a8980ed11940fbe97fe0f5d7abf
JM2311 <juan_morales2311@hotmail.com>
2022-10-01 15:09:56 -0300

    solucionando el issue de limitar caracteres del modelo usuario y peliculas

--- models/contenidoCategoriasModel.js
+++ models/contenidoCategoriasModel.js
@@ -2,17 +2,42 @@ const { Schema, model } = require("mongoose");
 
 const listContenido = Schema({
   id: {
-    
+    type: Number,
+    minLength: 6,
+    maxLength: 6,
+    require: true,
   },
-  titulo: String,
-  fecha: String,
-  descripcion: String,
+  titulo: {
+    type: String,
+    minLength: 4,
+    maxLength: 25,
+    require: true,
+  },
+  fecha: {
+    type: String,
+    minLength: 10,
+    maxLength: 10,
+    require: true,
+  },
+  descripcion: {
+    type: String,
+    minLength: 10,
+    maxLength: 300,
+    require: true,
+  },
   categoria: {
     type: String,
     default: String,
+    minLength: 4,
+    maxLength: 15,
     require: true,
   },
-  poster: String,
+  poster: {
+    type: String,
+    minLength: 10,
+    maxLength: 100,
+    require: true,
+  },
 });
 
 const contenidoCategorias = model("contenidoCategorias", listContenido);
